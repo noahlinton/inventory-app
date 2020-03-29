8d87c0a7ac519da58d1b941c2121c1e3dd3a70d7
Noah Linton <nlinton123@hotmail.com>
2020-03-28 21:19:54 -0400

    setting up project

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import DenseTable from "./table.js";
+
+function App() {
+  return (
+    <div className="App">
+      <DenseTable />
+    </div>
+  );
+}
+
+export default App;
