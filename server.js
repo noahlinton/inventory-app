cd91358669189517c2a39e0f05980bb23fbe38ac
Noah Linton <nlinton123@hotmail.com>
2020-03-30 15:04:29 -0400

    removed sensitive info

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ const PORT = process.env.PORT || 8080;
 
 const routes = require('./routes/api');
 
-//const MONGODB_URI =
-//"mongodb+srv://lintonn:x9S6Nm8cJxAX37m@inventoryapp-b0oan.mongodb.net/test?retryWrites=true&w=majority";
-
-//mongodb+srv://lintonn:x9S6Nm8cJxAX37m@inventoryapp-b0oan.mongodb.net/test?retryWrites=true&w=majority
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/inventory", {
   useNewUrlParser: true,
   useUnifiedTopology: true
