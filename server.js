2deabe62cf9fde63f700d173bf589c748e62daa7
Noah Linton <nlinton123@hotmail.com>
2020-03-30 14:56:01 -0400

    fixing heroku hopefully

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const routes = require('./routes/api');
 //const MONGODB_URI =
 //"mongodb+srv://lintonn:x9S6Nm8cJxAX37m@inventoryapp-b0oan.mongodb.net/test?retryWrites=true&w=majority";
 
+//mongodb+srv://lintonn:x9S6Nm8cJxAX37m@inventoryapp-b0oan.mongodb.net/test?retryWrites=true&w=majority
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/inventory", {
   useNewUrlParser: true,
   useUnifiedTopology: true
